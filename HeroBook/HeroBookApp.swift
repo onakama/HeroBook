c340e4ab6f874d188f14f560fc2f0d6783aa20db
onakama <80329420+onakama@users.noreply.github.com>
2023-01-15 11:59:08 +0900

    Initial Commit

---
+++ HeroBook/HeroBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeroBookApp.swift
+//  HeroBook
+//
+//  Created by onakama on 2023/01/15.
+//
+
+import SwiftUI
+
+@main
+struct HeroBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
